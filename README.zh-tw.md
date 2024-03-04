4a182212b086440e226c2dc7f6f2e07189295667
Mosil <mosil.biz@gmail.com>
2024-03-05 00:01:50 +0800

    update code

--- README.zh-tw.md
+++ README.zh-tw.md
@@ -5,21 +5,29 @@
 此 Flutter 專案在展示如何使用 Clean Architecture 來整合 Google Gemini API <br/>
 
 - [Clean Architecture](https://blog.cleancoder.com/uncle-bob/2012/08/13/the-clean-architecture.html)
-為 Uncle Bob 於 2012 發佈在其部落格之軟體架構。
+  為 Uncle Bob 於 2012 發佈在其部落格之軟體架構。
 
 ## 如何開始
 
 ### 取得 API KEY
 
-1. 請到 [Google AI Studio](https://aistudio.google.com/app/apikey) 取得
-   apiKey
-2. 於 `lib/core/constants.dart` 中置換掉 `apiKey`
+1. 請到 [Google AI Studio](https://aistudio.google.com/app/apikey) 取得 apiKey。
+2. 於 `lib/core/constants.dart` 中置換掉 `apiKey`。
 
 ### 分支
 
-此專案有兩個分支：
-1. main
-   初始的樣態，更換 api key 即可以執行。
+此專案有三個分支，
+
+1. `main`，初始的樣態，更換 api key 即可以執行。
+2. `1-empty-folders`，以 `main` 分支為基礎，加入架構所需的空目錄。
+3. `2-completed`，完成程式碼於整個目錄中的分類。
+
+## 備註
+
+1. 架構是一個原則！
+2. 至於目錄的分法、名稱，甚至是程式中要怎麼調整沒有固定答案，
+3. 團隊只要統一規範，讓後續易於維護即可~
+4. 此說明文字的英文版皆使用 Google Gemini 協助。
 
 ## Credits
 
@@ -29,7 +37,7 @@ Developer - Ada/阿達 (@mosil)
 
 The MIT License (MIT)
 
-Copyright (c) 2024 Mosil Studio
+Copyright (c) 2024 [Mosil Studio](https://mosil.space/)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
--- lib/ui/pages/chat_page.dart
+++ lib/ui/pages/chat_page.dart
@@ -3,7 +3,6 @@ import 'package:gemini_clean_architecture/core/constants.dart';
 import 'package:gemini_clean_architecture/models/message_model.dart';
 import 'package:gemini_clean_architecture/models/message_type.dart';
 import 'package:gemini_clean_architecture/ui/widgets/loading_view.dart';
-import 'package:gemini_clean_architecture/ui/widgets/message_list_view.dart';
 import 'package:gemini_clean_architecture/ui/widgets/send_area_view.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
@@ -15,7 +14,8 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  final List<MessageModel> _messages = [];
+  final List<MessageModel> _list = [];
+  final ScrollController _controller = ScrollController();
   final _model = GenerativeModel(model: modelName, apiKey: apiKey);
   bool _isLoading = false;
 
@@ -34,8 +34,13 @@ class _ChatPageState extends State<ChatPage> {
                   child: SelectionArea(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: MessageListView(
-                        list: _messages,
+                      child: ListView.builder(
+                        itemCount: _list.length,
+                        controller: _controller,
+                        itemBuilder: (context, index) {
+                          final item = _list[index];
+                          return _buildMessageItem(context, item);
+                        },
                       ),
                     ),
                   ),
@@ -60,14 +65,45 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
+  _buildMessageItem(BuildContext context, MessageModel item) {
+    final maxWidth = MediaQuery.of(context).size.width * 0.8;
+    return Row(
+      mainAxisAlignment: item.type.isSelf ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: LimitedBox(
+              maxWidth: maxWidth,
+              child: Text(item.text),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _controller.animateTo(
+        _controller.position.maxScrollExtent,
+        duration: const Duration(
+          milliseconds: 500,
+        ),
+        curve: Curves.linear,
+      ),
+    );
+  }
+
   _sendPrompt(String text) async {
     final content = [Content.text(text)];
     setState(() {
       _isLoading = true;
+      _scrollDown();
       // 將自己傳出的訊息呈現在 list 上
       // Show the outgoing text message in a list
       final self = MessageModel(type: MessageType.self, text: text);
-      _messages.add(self);
+      _list.add(self);
     });
 
     try {
@@ -76,10 +112,11 @@ class _ChatPageState extends State<ChatPage> {
       final response = await _model.generateContent(content);
       setState(() {
         _isLoading = false;
+        _scrollDown();
         // 將收到的結果呈現在 list 上
         // Display the received results in the list view
         final result = MessageModel(type: MessageType.feedback, text: response.text ?? "");
-        _messages.add(result);
+        _list.add(result);
       });
     } catch (e) {
       // 我們傳送出去的 prompt 可能會發生無法獲得結果的狀況，這個結果不會放在 response.text 中；它有機會被用 throw 傳出，因此用 catch 來補獲會是個比較安全的做法
